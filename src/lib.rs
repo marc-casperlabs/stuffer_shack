fccebe5a51721e81954657104822b10b51e1d71e
Marc Brinkmann <marc@casperlabs.io>
2022-01-28 10:51:48 +0000

    Store length as data

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,13 @@ use memmap::{MmapMut, MmapOptions};
 // TODO: Persist write offset.
 
 const MAP_SIZE: usize = usize::MAX / 2;
+type Length = u32;
+const LENGTH_SIZE: usize = mem::size_of::<Length>();
 
 #[derive(Debug)]
 struct StufferShack<K> {
     /// Maps a key to an offset.
-    index: HashMap<K, (usize, usize)>,
+    index: HashMap<K, usize>,
     write_offset: AtomicUsize,
     data: MmapMut,
 }
@@ -47,30 +49,58 @@ impl<K> StufferShack<K> {
 
 impl<K> StufferShack<K>
 where
-    K: Eq + Hash,
+    // TODO: Cleanup traits, `K` needs to be a fixed size POD.
+    K: Copy + Eq + Hash + AsRef<[u8]>,
 {
     // TODO: Allow parallel writes.
     fn write(&mut self, key: K, value: &[u8]) {
+        assert!(
+            self.index.get(&key).is_none(),
+            "rewriting keys is not supported"
+        );
+
         // Determine where to write the data.
         // TODO: See if ordering can be relaxed.
+        // TODO: Ensure conversion is not lossy.
         let value_len = value.len();
-        let start = self
-            .write_offset
-            .fetch_add(value_len, std::sync::atomic::Ordering::SeqCst);
-        let end = start + value_len;
+        assert!(value_len <= Length::MAX as usize);
 
-        // Write data.
-        let dest = &mut self.data[start..end];
-        dest.copy_from_slice(value);
+        // Format: LENGTH(32) + KEY + VALUE
+        let total_len = LENGTH_SIZE + mem::size_of::<K>() + value_len;
+
+        let start_len = self
+            .write_offset
+            .fetch_add(total_len, std::sync::atomic::Ordering::SeqCst);
+
+        let start_key = start_len + LENGTH_SIZE;
+        let start_value = start_key + mem::size_of::<K>();
+
+        // TODO: Optimize range checks.
+        // Write length, key, data.
+        let slice_len = &mut self.data[start_len..start_key];
+        slice_len.copy_from_slice(&(value_len as u32).to_le_bytes());
+
+        let slice_key = &mut self.data[start_key..start_value];
+        slice_key.copy_from_slice(key.as_ref());
+
+        let slice_value = &mut self.data[start_value..(start_value + value_len)];
+        slice_value.copy_from_slice(value);
 
         // Update index.
-        assert!(self.index.get(&key).is_none());
-        self.index.insert(key, (start, end));
+        self.index.insert(key, start_len);
     }
 
     fn read(&self, key: &K) -> Option<&[u8]> {
-        let (start, end) = self.index.get(key)?;
-        let data = &self.data[*start..*end];
+        let start = *self.index.get(key)?;
+
+        let value_length_slice = &self.data[start..(start + LENGTH_SIZE)];
+        let value_length = Length::from_le_bytes(value_length_slice.try_into().unwrap()) as usize;
+
+        // We skip the key (although we could check it here if wanted to...)
+        let value_start = start + LENGTH_SIZE + mem::size_of::<K>();
+        let value_end = value_start + value_length;
+
+        let data = &self.data[value_start..value_end];
         Some(data)
     }
 }
