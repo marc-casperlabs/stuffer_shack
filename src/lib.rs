84547a7cf4d013c375953e42147f8a3eec827565
Marc Brinkmann <marc@casperlabs.io>
2022-01-28 12:43:32 +0000

    Restore index on load

--- src/lib.rs
+++ src/lib.rs
@@ -14,22 +14,35 @@ use memmap::{MmapMut, MmapOptions};
 // TODO: Use im-rs for parallel read/write.
 // TODO: Use serialization of in-memory index, storing offset, to allow fast recovery of WAL.
 // TODO: Persist write offset.
+// TODO: Consider packing.
 
 const MAP_SIZE: usize = usize::MAX / 2;
 type ItemLen = u32;
 type DbLen = u64;
 const ITEM_LEN_SIZE: usize = mem::size_of::<ItemLen>();
 const DB_LEN_SIZE: usize = mem::size_of::<DbLen>();
-const DB_HEADER_SIZE: usize = DB_LEN_SIZE;
+const MAGIC_BYTES: [u8; 64] = [
+    b'S', b'T', b'U', b'F', b'F', b'E', b'R', b'_', b'S', b'H', b'A', b'C', b'K', b'_', b'_', b'_',
+    b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_',
+    b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_',
+    b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_', b'_',
+];
+const MAGIC_BYTES_LEN: usize = 64;
+const DB_HEADER_SIZE: usize = DB_LEN_SIZE + MAGIC_BYTES_LEN;
 
 #[derive(Debug)]
 struct StufferShack<K> {
     /// Maps a key to an offset.
     index: HashMap<K, DbLen>,
+    /// Internal data map.
     data: MmapMut,
 }
 
-impl<K> StufferShack<K> {
+impl<K> StufferShack<K>
+where
+    // TODO: Cleanup traits, `K` needs to be a fixed size POD.
+    K: Copy + Eq + Hash + AsRef<[u8]>,
+{
     fn open_disk<P: AsRef<Path>>(db: P) -> io::Result<Self> {
         let backing_file = fs::OpenOptions::new()
             .read(true)
@@ -49,66 +62,51 @@ impl<K> StufferShack<K> {
         Self::new(data)
     }
 
-    fn new(data: MmapMut) -> io::Result<Self> {
-        // TODO: Restore index.
-        Ok(StufferShack {
-            index: Default::default(),
-            data,
-        })
-    }
-}
+    fn new(mut data: MmapMut) -> io::Result<Self> {
+        let header = &mut data[0..DB_HEADER_SIZE];
+        let mut index = HashMap::new();
+        if &header[0..MAGIC_BYTES_LEN] != &MAGIC_BYTES[..] {
+            // Database not initialized, write the magic bytes and initial length.
+            header[0..MAGIC_BYTES_LEN].copy_from_slice(&MAGIC_BYTES);
+            let initial_len: DbLen = 0;
+            header[MAGIC_BYTES_LEN..].copy_from_slice(&initial_len.to_le_bytes());
+        } else {
+            // We're already initialized, so walk entire data to restore the index.
+            let total_size = store_length(&data) as usize;
+            let mut cur = DB_HEADER_SIZE;
+            while cur < total_size {
+                let record = load_record::<K>(&data, cur as u64);
+                // length, hash, data. We only need the hash.
+                // TODO: Unsafe-cast record header instead.
+                let hash_bytes = &record[ITEM_LEN_SIZE..(ITEM_LEN_SIZE + mem::size_of::<K>())];
 
-impl<K> StufferShack<K>
-where
-    // TODO: Cleanup traits, `K` needs to be a fixed size POD.
-    K: Copy + Eq + Hash + AsRef<[u8]>,
-{
-    /// Retrieves the length of the db without header from the db header.
-    fn store_length(&self) -> DbLen {
-        DbLen::from_le_bytes(self.data[0..DB_LEN_SIZE].try_into().unwrap())
+                // TODO: Find something better (moot with record header).
+                let hash_ptr: *const K = hash_bytes.as_ptr() as *const K;
+                let hash = unsafe { *hash_ptr };
+
+                index.insert(hash, cur as DbLen);
+            }
+        }
+
+        Ok(StufferShack { index, data })
     }
 
     /// Store the length of the db without the header in the db header.
     fn write_store_length(&mut self, size: DbLen) {
-        let dest = &mut self.data[0..DB_LEN_SIZE];
+        let dest = &mut self.data[MAGIC_BYTES_LEN..(MAGIC_BYTES_LEN + DB_LEN_SIZE)];
         dest.copy_from_slice(&size.to_le_bytes());
-    }
-
-    /// Converts a database offset into a memory offset, which includes the header.
-    fn data_offset_to_memory_offset(offset: DbLen) -> usize {
-        offset as usize + DB_HEADER_SIZE
-    }
-
-    fn calc_record_len(value_len: ItemLen) -> DbLen {
-        (ITEM_LEN_SIZE + mem::size_of::<K>() + value_len as usize)
-            .try_into()
-            .unwrap()
     }
 
     /// Reserves a record in the db with the specified size.
     ///
     /// Returns the data offset and a writable slice.
     fn reserve_record(&mut self, record_size: ItemLen) -> (DbLen, &mut [u8]) {
-        let old_store_length = self.store_length();
+        let old_store_length = store_length(&self.data);
         let new_store_length = old_store_length + record_size as DbLen;
         self.write_store_length(new_store_length);
-        let data = &mut self.data[Self::data_offset_to_memory_offset(old_store_length)
-            ..Self::data_offset_to_memory_offset(new_store_length)];
+        let data = &mut self.data[data_offset_to_memory_offset(old_store_length)
+            ..data_offset_to_memory_offset(new_store_length)];
         (old_store_length, data)
-    }
-
-    /// Loads a record from a specified offset.
-    fn load_record(&self, data_offset: DbLen) -> &[u8] {
-        // Read the length.
-        let mem_offset = Self::data_offset_to_memory_offset(data_offset);
-        let value_len = ItemLen::from_le_bytes(
-            self.data[mem_offset..(mem_offset + ITEM_LEN_SIZE)]
-                .try_into()
-                .unwrap(),
-        );
-        let record_len = Self::calc_record_len(value_len);
-
-        &self.data[mem_offset..(mem_offset + record_len as usize)]
     }
 
     // TODO: Allow parallel writes.
@@ -125,7 +123,7 @@ where
         assert!(value_len <= ItemLen::MAX as usize);
 
         // Format: LENGTH(32) + KEY + VALUE
-        let record_len = Self::calc_record_len(value_len as u32);
+        let record_len = calc_record_len::<K>(value_len as u32);
         let (record_offset, record) = self.reserve_record(record_len.try_into().unwrap());
 
         // TODO: Optimize range checks.
@@ -143,11 +141,46 @@ where
 
     fn read(&self, key: &K) -> Option<&[u8]> {
         let data_offset = *self.index.get(key)?;
-        let record = self.load_record(data_offset);
+        let record = load_record::<K>(&self.data, data_offset);
 
         let value_slice = &record[(ITEM_LEN_SIZE + mem::size_of::<K>())..];
         Some(value_slice)
     }
+}
+
+/// Retrieves the length of the db without header from the db header.
+fn store_length(data: &MmapMut) -> DbLen {
+    DbLen::from_le_bytes(
+        data[MAGIC_BYTES_LEN..(MAGIC_BYTES_LEN + DB_LEN_SIZE)]
+            .try_into()
+            .unwrap(),
+    )
+}
+
+/// Converts a database offset into a memory offset, which includes the header.
+fn data_offset_to_memory_offset(offset: DbLen) -> usize {
+    offset as usize + DB_HEADER_SIZE
+}
+
+/// Loads a record from a specified offset.
+fn load_record<K>(data: &MmapMut, data_offset: DbLen) -> &[u8] {
+    // Read the length.
+    let mem_offset = data_offset_to_memory_offset(data_offset);
+    let value_len = ItemLen::from_le_bytes(
+        data[mem_offset..(mem_offset + ITEM_LEN_SIZE)]
+            .try_into()
+            .unwrap(),
+    );
+    let record_len = calc_record_len::<K>(value_len);
+
+    &data[mem_offset..(mem_offset + record_len as usize)]
+}
+
+/// Calculates the length of a record from its data length (by adding key + local header size).
+fn calc_record_len<K>(value_len: ItemLen) -> DbLen {
+    (ITEM_LEN_SIZE + mem::size_of::<K>() + value_len as usize)
+        .try_into()
+        .unwrap()
 }
 
 #[cfg(test)]
